c9f5cc3b37dae05f40a6787da5f6f01cc5ea4349
oleksiykovtun <oleksiy.vt@gmail.com>
2015-05-02 09:00:59 +0300

    User photos from gallery

--- app/src/main/java/com/oleksiykovtun/iwmy/speeddating/android/fragments/common/ProfileEditFragment.java
+++ app/src/main/java/com/oleksiykovtun/iwmy/speeddating/android/fragments/common/ProfileEditFragment.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -25,7 +27,9 @@ import java.io.File;
 public abstract class ProfileEditFragment extends AppFragment {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    private Uri cameraPhotoUri;
+    static final int REQUEST_IMAGE_FROM_GALLERY = 2;
+
+    private Uri photoUri;
     private Bitmap photoBitmap = null;
     private Bitmap thumbnailBitmap = null;
 
@@ -40,19 +44,7 @@ public abstract class ProfileEditFragment extends AppFragment {
             case R.id.button_photo:
                 if (((Button)view).getText().equals(getText(R.string.button_choose_photo))) {
                     // setting photo
-                    try {
-                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        final String tempPhotoName = "temp_photo.jpg";
-                        new File(getActivity().getFilesDir(), tempPhotoName).delete();
-                        getActivity().openFileOutput(tempPhotoName, Context.MODE_WORLD_WRITEABLE)
-                                .close(); // todo replace
-                        cameraPhotoUri = Uri.fromFile(new File(getActivity().getFilesDir(),
-                                tempPhotoName));
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, cameraPhotoUri);
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    } catch (Throwable e) {
-                        showToast(R.string.message_camera_error);
-                    }
+                    openMenu(view);
                 } else {
                     // removing photo
                     getImageView(R.id.image_user_pic).setImageResource(R.drawable.no_photo_invisible);
@@ -69,19 +61,61 @@ public abstract class ProfileEditFragment extends AppFragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == getActivity().RESULT_OK) {
+        if (resultCode == getActivity().RESULT_OK &&
+                (requestCode == REQUEST_IMAGE_CAPTURE || requestCode == REQUEST_IMAGE_FROM_GALLERY)) {
             try {
+                if (requestCode == REQUEST_IMAGE_FROM_GALLERY) {
+                    photoUri = data.getData();
+                }
                 Bitmap rawBitmap = MediaStore.Images.Media.getBitmap(
-                        getActivity().getContentResolver(), cameraPhotoUri);
+                        getActivity().getContentResolver(), photoUri);
                 photoBitmap = ImageManager.scaleBitmapToMinSideSize(rawBitmap, 640);
                 thumbnailBitmap = ImageManager.cropCenterSquare(ImageManager
                         .scaleBitmapToMinSideSize(rawBitmap, 80));
                 getImageView(R.id.image_user_pic).setImageBitmap(photoBitmap);
                 ((Button) getViewById(R.id.button_photo)).setText(R.string.button_remove_photo);
             } catch (Throwable e) {
-                showToast(R.string.message_camera_error);
+                showToast(R.string.message_photo_getting_error);
             }
         }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_from_camera:
+                try {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    final String tempPhotoName = "temp_photo.jpg";
+                    new File(getActivity().getFilesDir(), tempPhotoName).delete();
+                    getActivity().openFileOutput(tempPhotoName, Context.MODE_WORLD_WRITEABLE)
+                            .close(); // todo replace
+                    photoUri = Uri.fromFile(new File(getActivity().getFilesDir(),
+                            tempPhotoName));
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                } catch (Throwable e) {
+                    showToast(R.string.message_photo_getting_error);
+                }
+                break;
+            case R.id.menu_from_gallery:
+                try {
+                    Intent selectPictureIntent = new Intent(Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(selectPictureIntent, REQUEST_IMAGE_FROM_GALLERY);
+                } catch (Throwable e) {
+                    showToast(R.string.message_photo_getting_error);
+                }
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getActivity().getMenuInflater().inflate(R.menu.choose_photo, menu);
     }
 
 
